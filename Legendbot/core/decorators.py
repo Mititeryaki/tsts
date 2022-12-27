eeb5678b7f1490a6d4525850bdb2543482942944
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-12-27 17:05:04 +0530

    Update decorators.py

--- Legendbot/core/decorators.py
+++ Legendbot/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "Only My Master can Access This !!\n\nDeploy your own Legendbot."
+                or "Only My Master can Access This !!\n\nDeploy your own @Legendbot_XD."
             )
             await c_q.answer(
                 HELP_TEXT,
