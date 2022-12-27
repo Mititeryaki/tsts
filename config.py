1271315047588ea7d1e272e8ce8579dc3a6b9962
Mititeryaki <113138869+Mititeryaki@users.noreply.github.com>
2022-12-27 22:33:59 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,21 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 8460373
-    API_HASH = "83d8e423197251216303abfcbed9e820"
+    APP_ID = 27090504
+    API_HASH = "47414bd130fcdf88b5546b116a779058"
     # the name to display in your alive message
-    ALIVE_NAME = "LegendBoy"
+    ALIVE_NAME = "mitigangbottel"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://oxkxqagp:846JSW5Jb4sbPZNlowgqkS_HYfWOFyoa@snuffleupagus.db.elephantsql.com/oxkxqagp"
+    DB_URI = "postgres://bytclqqc:Ni3b-Op06yuxW3W7m0Nj92jJq0FBZm5v@mel.db.elephantsql.com/bytclqqc"
     # Extermnal Plugins
     EXTERNAL_REPO = False
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    LEGEND_STRING = "1BVtsOGcBu3ws8fazY8lm0G69p74TEjWIpLTbio2Qb0RpZfD5oqgxl1vM2ReCBKKD53u2VkBthC9NeFJAN2r7tzjrJyb0_vXCF6X-yb0vLi11CAWT8-BAQVih_0Uw4EI_W9CvyNwZYHN-rRYROqFDsj766wnuHfaepxl3lAFRm-DZ4e9wKNs9JP3JV_zo4xWfrFu8_GbLYD8tUtAOKjWU4bREXMBvwS96TX7sItXoLomoStJOgwGrBEdufXe_zIwGYBdzzXUFyI6WGNXvMdqza1-JMRpawihXDV9QQgL5IUAuZBbT0M5jbdK-khTo3JfmU6ykDk09_q1dTWost3JhdgGdC7DiQ5Q="
+    LEGEND_STRING = "BACBmr2CH49CHDwLypaF5OpKqGupvOZfzfJkGYpSFPE1rj0nWxiyl99FLgTRU3URFXMPxlcfZOQqVatGOlkfCBehv59hPK4rFJ3fEcEA9Fj0z3QbJPv0ZzaLFIk9Pv3QDSRnOn0xQeRXVo4dhjI6CUCtpa6dMbIkdlLJ5tHvrjI1U6Uzgj93EPwOcPSqaOjuNS76aiuofOCaElFFCmAn29_K0lGyBou6JKXYJJECWEzRUilGpJZc8HrBG8-QdkeOa2R5qDzhnnAD2Umr-Kxkzp3Gqh9ZgcHMKdkqt1Axw2yfBHvXFs0AVLvFwFN5ERPKYQpySrg4vtTqwj0GNLGrEN2dAAAAAVDnzhAA"
     # Get in from bot father
-    BOT_TOKEN = "5132044795:AAHXkYsT1HLvPOEsNN3B7l9tekJvrVZknwg"
+    BOT_TOKEN = "5910828786:AAGk4kH0zYSiEFHmG2axeWf1wjGBdCk25ng"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     # PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
     HANDLER = "."
     # command hanler for sudo
-    SUDO_HANDLER = "."
+    SUDO_HANDLER = "5652336144"
