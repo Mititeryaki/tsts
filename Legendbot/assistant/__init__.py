55297b82ce07659f7fabebf89c35bca3c587f6d0
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-12-27 16:28:03 +0530

    Update __init__.py

--- Legendbot/assistant/__init__.py
+++ Legendbot/assistant/__init__.py
@@ -1,4 +1,9 @@
-from Legendbot import BOTLOG, BOTLOG_CHATID, legend
+from Legendbot import BOTLOG, BOTLOG_CHATID
+from Legendbot.core.session import legend, tgbot
 
 from ..Config import Config
 from ..core.inlinebot import *
+
+LEGEND_USER = legend.me.first_name
+The_LegendBoy = legend.uid
+legend_mention = f"[{LEGEND_USER}](tg://user?id={The_LegendBoy})"
